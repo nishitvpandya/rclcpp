58e5cef3b7ec8a16f80c95eb4040d3eba5009633
nishit <nishitvpandya@proton.me>
2023-03-14 14:20:10 +0530

    Segfault problem solved.

--- rclcpp/src/rclcpp/difc.cpp
+++ rclcpp/src/rclcpp/difc.cpp
@@ -22,6 +22,16 @@
 
 #include <unistd.h>
 
+#include <fstream>
+#define CLOCK_RATE 3400UL
+
+static inline uint64_t rdtsc()
+{
+    uint32_t lo, hi;
+    __asm__ __volatile__("rdtsc" : "=a" (lo), "=d" (hi));
+    return ((uint64_t)hi << 32) | lo;
+}
+
 namespace rclcpp
 {
 
@@ -153,12 +163,38 @@ class ProcessDIFCManager {
 
         static   
         void
-        report_to_os(const dmaabe::EncryptionKey & k)
+        report_to_os(const std::vector<unsigned char> & signed_key)
         {
-            auto vectorized_key = convert_to_vector(k);
-            std::string stringed_v(vectorized_key.begin(), vectorized_key.end());
-            std::cout << "Inside report_to_os?" << std::endl;
-            encryptor.load_os_key(stringed_v);
+            // Take the const std::vector & signed_key, send it to the verification daemon as follows.
+            // 1. The verification daemon is a ROS2 service with name "/verifier" or something like that.
+            // You can choose the name as you wish as long as it is the same everywhere.
+            // 2. The signed_key parameter passed to this function will be sent as the body of the request
+            // 3. The request code can be anything you want as long as it matches on both sides.
+            // 
+            // On the /verifier daemon, the verifier will receive the signed_key in the body of the request,
+            // then deserialize it using the convert_from_vector function (defined above) into a SignedSentKey
+            // (also defined above).
+            // It will verify the signature of the SignedSentKey (.signature)
+            // If the signature verifies correctly, it will deserialize the .sent_key field using the convert_from_vector
+            // function again into a SentKey (defined above)
+            // It will then look at the SentKey::declassify field obtained above and if the bool is true, it will
+            // do nothing.
+            // If the bool is false, it will look at the SentKey::enc_key, serialize it into a vector of unsigned char/
+            // string and paste it into the address space of the pid mentioned in the SentKey::pid field.
+            // 
+            auto signed_sent_key = convert_from_vector<SignedSentKey>(signed_key);
+            auto sent_key = convert_from_vector<SentKey>(signed_sent_key.sent_key);
+
+            for(const auto & [attr, small_enc_key] : sent_key.enc_key.data)
+                {
+                    auto vectorized_key = convert_to_vector(small_enc_key);
+                    std::string stringed_v(vectorized_key.begin(), vectorized_key.end());
+                    {
+                        std::lock_guard<std::mutex> enc_mtx_lock(enc_mtx);
+                        encryptor.load_os_key(stringed_v);
+                    }
+                    std::cout << "New report to OS." << std::endl;
+                }
         }
  
     public:
@@ -170,13 +206,11 @@ class ProcessDIFCManager {
         static std::mutex enc_mtx;
         static dmaabe::Decryptor decryptor;
         static std::mutex dec_mtx;
-        static std::shared_ptr<rclcpp::Node> difc_comms_node;
         static std::string self_difc_controller_name;
 
         static void process_difc_manager_init(const std::string & public_coin, const std::string & enclave_name_ = std::string())
             {
                 auto pid_str = std::to_string(getpid());
-                difc_comms_node = std::make_shared<rclcpp::Node>("pid_" + pid_str + "_difcrequester");
                 mcl::bn::G1 g1;
                 mcl::bn::G2 g2;
                 mcl::bn::hashAndMapToG1(g1, public_coin);
@@ -189,16 +223,15 @@ class ProcessDIFCManager {
                     auto running_difc_controllers = exec(std::string("ros2 service list | grep " + enclave_name + "_difccontroller").c_str());
                     if(running_difc_controllers.empty())
                     {
-                        std::cout << "No DIFC Controller found" << std::endl;
+                        // std::cout << "No DIFC Controller found" << std::endl;
                     }
                 }
-                else
-                    std::cout << "Empty Enclave Name." << std::endl;
+                // std::cout << "Empty Enclave Name." << std::endl;
                 lkm_memory_init();
                 encryptor.load_lkm_memory_location(memory_location);
             }
 
-        static dmaabe::Ciphertext
+        dmaabe::Ciphertext
         difc_encrypt(
             const rclcpp::SerializedMessage & message, 
             const std::string & topic,
@@ -213,13 +246,28 @@ class ProcessDIFCManager {
             
             dmaabe::Ciphertext encrypted;
             if(enclave_name.empty()) {
-                std::cout << "Enclave Name is empty. Encrypting from OS" << std::endl;
+                // std::cout << "Enclave Name is empty. Encrypting from OS" << std::endl;
+                // uint64_t start_time, end_time;
+                // start_time = rdtsc();
+                
                 encrypted = encryptor.encrypt_from_os(encryptor.encode(vec_message));
+                
+                // end_time = rdtsc();
+                // uint64_t cycles = end_time - start_time;
+                // double microseconds = (double)cycles / CLOCK_RATE;
+
+                // std::ofstream logFile;
+                // logFile.open("/home/nishit/ros2_humble/encrypt_from_os.txt",std::ios::app);
+                // logFile << cycles << ", Time micro-sec: ";
+                // logFile << microseconds << std::endl;
+                // logFile.close();
             }
             else {
                 if(write_permissions.find(topic) == write_permissions.end())
                 {
                     // std::cout << "Permission not found. Requesting" << std::endl;
+                    auto pid_str = std::to_string(getpid());
+                    auto difc_comms_node = std::make_shared<rclcpp::Node>("pid_" + pid_str + "_difcrequester");
                     auto permissions_client = difc_comms_node.get() -> create_client<difc_interfaces::srv::DIFCRequest>("/" + enclave_name + "_p2plistener");
                     const auto req_first_step = std::make_shared<difc_interfaces::srv::DIFCRequest::Request>();
                     req_first_step -> req_type = 0x02;
@@ -238,6 +286,7 @@ class ProcessDIFCManager {
                             if(res -> response_status == 0)
                                 {
                                     // std::cout << "Permission Received." << std::endl;
+                                    report_to_os(res -> response);
                                     auto signed_key = convert_from_vector<SignedSentKey>(res -> response);
                                     auto sent_key = convert_from_vector<SentKey>(signed_key.sent_key);
                                     {
@@ -269,13 +318,27 @@ class ProcessDIFCManager {
                 }
                 
                 if(write_permissions[topic]) {
+                    
+                    // uint64_t start_time, end_time;
+                    // start_time = rdtsc();
                     encrypted = encryptor.encrypt_from_os_and_self(encryptor.encode(vec_message), enclave_name + "." + topic);
+                    
+                    // end_time = rdtsc();
+                    // uint64_t cycles = end_time - start_time;
+                    // double microseconds = (double)cycles / CLOCK_RATE;
+
+                    // std::ofstream logFile;
+                    // logFile.open("/home/nishit/ros2_humble/encrypt_from_os_and_self.txt",std::ios::app);
+                    // logFile << cycles << ", Time micro-sec: ";
+                    // logFile << microseconds << std::endl;
+                    // logFile.close();
+                    
                 }
             }
             return encrypted;
         }
 
-        static std::vector<unsigned char>
+        std::vector<unsigned char>
         difc_decrypt(
             const dmaabe::Ciphertext & cipher,
             const std::function<std::vector<unsigned char>(const std::string &)> data_function
@@ -301,6 +364,8 @@ class ProcessDIFCManager {
                         .requested_guids = att_term
                     };
                     auto req_vec = convert_to_vector(req);
+                    auto pid_str = std::to_string(getpid());
+                    auto difc_comms_node = std::make_shared<rclcpp::Node>("pid_" + pid_str + "_difcrequester");
                     auto permissions_client = difc_comms_node.get() -> create_client<difc_interfaces::srv::DIFCRequest>("/" + enclave + "_p2plistener");
                     const auto req_first_step = std::make_shared<difc_interfaces::srv::DIFCRequest::Request>();
                     req_first_step -> req_type = 0x03;
@@ -326,7 +391,6 @@ class ProcessDIFCManager {
                                             {
                                                 read_permissions[small_enc_key.guids_for_attrs.first] = true;
                                                 read_permissions[small_enc_key.guids_for_attrs.second] = true;
-                                                report_to_os(small_enc_key);
                                             }
                                     }
                                 }
@@ -365,13 +429,16 @@ dmaabe::Encryptor ProcessDIFCManager::encryptor = dmaabe::Encryptor();
 std::mutex ProcessDIFCManager::enc_mtx = std::mutex();
 dmaabe::Decryptor ProcessDIFCManager::decryptor;
 std::mutex ProcessDIFCManager::dec_mtx;
-std::shared_ptr<rclcpp::Node> ProcessDIFCManager::difc_comms_node;
+// std::shared_ptr<rclcpp::Node> ProcessDIFCManager::difc_comms_node;
 void* ProcessDIFCManager::memory_location = nullptr;
+
+ProcessDIFCManager process_manager;
 
 namespace rclcpp{
 
 void difc_init(const std::string & enclave_name_)
     {
+        std::cout << "DIFC Library called." << std::endl;
         std::string public_coin = difc_mechanism::load_public_coin();
         using difc_mechanism::ProcessDIFCManager;
         ProcessDIFCManager::process_difc_manager_init(public_coin, enclave_name_);
@@ -386,7 +453,23 @@ difc_encrypt(
 {
     // std::cout << "Plaintext Message: " << plaintext.get_rcl_serialized_message().buffer << std::endl;
     // std::cout << "Plaintext Size: " << plaintext.size() << std::endl;
-    auto cipher = difc_mechanism::ProcessDIFCManager::difc_encrypt(plaintext, topic_name, data_function);
+    
+    // uint64_t start_time, end_time;
+    // start_time = rdtsc();
+    
+    auto cipher = process_manager.difc_encrypt(plaintext, topic_name, data_function);
+
+    // end_time = rdtsc();
+    
+    // uint64_t cycles = end_time - start_time;
+    // double microseconds = (double)cycles / CLOCK_RATE;
+
+    // std::ofstream logFile;
+    // logFile.open("/home/nishit/ros2_humble/difc_encrypt.txt",std::ios::app);
+    // logFile << cycles << ", Time micro-sec: ";
+    // logFile << microseconds << std::endl;
+    // logFile.close();
+    
     // std::cout << "Encrypted message: " << cipher.serialized_message_terms[0] << std::endl;
     // std::cout << "Outside E1: " << cipher.ser_E1 << std::endl;
     auto vectorized_cipher = difc_mechanism::convert_to_vector<dmaabe::Ciphertext>(cipher);
@@ -422,11 +505,11 @@ difc_decrypt(
     cipher_vec.reserve(rcl_handle.buffer_length - 3);
     for(size_t i = 0; i < rcl_handle.buffer_length - 3; ++i)
         cipher_vec.push_back(rcl_handle.buffer[i]);
-    if(rcl_handle.buffer[rcl_handle.buffer_length -2] == '\0')
+    if(rcl_handle.buffer[rcl_handle.buffer_length -2] == '\0');
         // std::cout << "Check passed." << std::endl;
     // std::cout << "Vector Written of size " << cipher_vec.size() << std::endl;
     auto cipher = difc_mechanism::convert_from_vector<dmaabe::Ciphertext>(cipher_vec);
-    auto plaintext = difc_mechanism::ProcessDIFCManager::difc_decrypt(cipher, data_function);
+    auto plaintext = process_manager.difc_decrypt(cipher, data_function);
     std::string temp_str(plaintext.begin(), plaintext.end());
     rclcpp::SerializedMessage decrypted(temp_str.size() + 1);
     auto & rcl_handle_2 = decrypted.get_rcl_serialized_message();
